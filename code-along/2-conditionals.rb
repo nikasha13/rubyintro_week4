b254669be2cd5db4a9bd47625ca01f1d05e810b3
Nikasha Patel <nikasha.patel@kellogg.northwestern.edu>
2025-04-22 14:32:01 +0000

    2-conditionals.rb completed with notes

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,49 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+true
+false
 
 # Boolean Expressions
+x = 3
+y = 5
+puts x == y
+puts x !=y 
+puts x >y
+puts x < y 
+puts x>= y 
+puts x<= y 
 
 # If Conditional Logic
+if x == y
+    puts "WHat?????"
+end
+#write what helps you - if these things are not the same, return "Not the same"
+if x !=y
+    puts "Not the same"
+end
 
 # If/Else Conditional Logic
+if x ==y
+    puts "WHatt???"
+else 
+    puts "phewww...math works"
+end
 
 # Elsif Conditional Logic
+if x == y 
+    puts "Whaaat???"
+elsif x > y
+    puts "this shouldn't work"
+else
+    puts "phewww... math works"
+end
 
 # Combining Expressions
+if x == y || x == 10
+    puts "WHat????"
+elsif x > y
+    puts "this shouldn't work"
+else
+    puts "phewww... math works"
+end
