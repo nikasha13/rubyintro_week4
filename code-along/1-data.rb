7bf9d5d599596e09d2030354089994853ee45570
Nikasha Patel <nikasha.patel@kellogg.northwestern.edu>
2025-04-22 14:11:28 +0000

    1-data.rb completed with notes

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,47 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts(3)
+puts 5
+puts 3+5
+puts 3*5
+puts 3-5
+puts 3/5
+puts 3/5.0
 
 # Non-numbers
+puts "unicorn"
 
-# True, False
+# True, False (must be lowercase)
+puts true
+puts false
 
 # Nothing
 
 # Variables
+x = 3
+y = 5
+puts x
+puts y 
+z = x*y
+puts z
+puts x + y
+puts x.class
+# variables can be reassigned
+z = 99
+puts z
 
 # Combine strings and variables
+#instead of...
+puts "Tacos on today's menu: "
+puts x
+#combine string and variable:
+puts "Tacos on today's menu: #{x}"
+puts "Tacos on today's menu: #{x+y}"
+amount = 3
+food = "Tacos"
+puts "#{food} on today's menu: #{amount}"
 
 # String manipulation
+puts food.reverse
+puts food.upcase
