7d809f87a7fe867d4b663fa8f3432d74369d74ff
Nikasha Patel <nikasha.patel@kellogg.northwestern.edu>
2025-04-23 20:48:08 +0000

    completed lab: 6-weather.rb

--- labs/6-weather.rb
+++ labs/6-weather.rb
@@ -16,17 +16,40 @@
 # Note: only display the chance of rain if above 50%.
 # Note: do not display the chance of rain if conditions are already rainy.
 weather_data = {
-  current: {
-    temperature: 67,
-    conditions: "Sunny"
+  "current"=> {
+    "temperature"=> 67,
+    "conditions"=> "Sunny"
   },
-  forecast: [
-    { temperature: 65, conditions: "Mostly Cloudy", precipitation: 0.4 },
-    { temperature: 70, conditions: "Partly Cloudy", precipitation: 0.2 },
-    { temperature: 62, conditions: "Sunny", precipitation: 0.1 },
-    { temperature: 68, conditions: "Sunny", precipitation: 0.1 },
-    { temperature: 65, conditions: "Partly Cloudy", precipitation: 0.7 },
-    { temperature: 72, conditions: "Partly Cloudy", precipitation: 0.4 },
-    { temperature: 60, conditions: "Rainy", precipitation: 0.9 }
+  "forecast"=> [
+    { "temperature"=> 65, "conditions"=> "Mostly Cloudy", "precipitation"=> 0.4 },
+    { "temperature"=> 70, "conditions"=> "Partly Cloudy", "precipitation"=> 0.2 },
+    { "temperature"=> 62, "conditions"=> "Sunny", "precipitation"=> 0.1 },
+    { "temperature"=> 68, "conditions"=> "Sunny", "precipitation"=> 0.1 },
+    { "temperature"=> 65, "conditions"=> "Partly Cloudy", "precipitation"=> 0.7 },
+    { "temperature"=> 72, "conditions"=> "Partly Cloudy", "precipitation"=> 0.4 },
+    { "temperature"=> 60, "conditions"=> "Rainy", "precipitation"=> 0.9 }
   ]
 }
+#Current Forecast
+current_temp = weather_data["current"]["temperature"]
+puts current_temp
+current_conditions = weather_data["current"]["conditions"]
+puts current_conditions
+puts "Currently it is #{current_temp} degrees and #{current_conditions}."
+
+#7 Day Forecast
+puts "Forecast for the next 7 days:"
+
+for day in weather_data["forecast"]
+  temp = day["temperature"]
+  conditions = day["conditions"]
+  precipitation = day["precipitation"]
+
+summary = "#{temp} degrees and #{conditions}"
+if conditions != "Rainy" && precipitation > 0.5
+  percent_rain = (precipitation*100).to_f
+  summary += "with a #{percent_rain}% chance of rain"
+end
+
+puts summary
+end
